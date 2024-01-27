5d85add581fa65a7728ec9fce756e0a3f7170a79
Bernardo Caroli <bernardo@snap.com.br>
2024-01-27 14:49:47 -0300

    modificaçoes no dia 1

--- dia01/02_dataframes.py
+++ dia01/02_dataframes.py
@@ -7,23 +7,24 @@ data = {
     "nome":["Téo", "Nah", "Maria", "José", "Marina", "Jessica", "InfoSlack"],
     "idade":[30, 33, 2, 45, 65, 43, 40],
     "cor": ["Preto", "Verde", "Azul", "Vermelho", "Amarelo", "Verde", "Azul"],
-    "renda": [3566, 1345, 0, 6576, 4325, 5326, 10244]
+    "renda": [3566, 1345, 0, 6576, 4325, 5326, 10244],
+    "signo": ['Virgem', 'Peixes', 'Sagitário', 'Peixes','Virgem', 'Peixes', 'Leão']
 }
 
 data["idade"]
 
 # %%
 df = pd.DataFrame(data)
-df["idade"]
+df['idade']
 
 # %%
-
-type(df["idade"])
+type(df["idade"]) #mostra que é uma série
 
 # %%
 
 df[['idade', 'renda']].describe()
+df[['idade', 'renda']].mean()
 
 # %%
 
-df.mean()
+df.info(memory_usage='deep')
--- dia01/04_navegando.py
+++ dia01/04_navegando.py
@@ -25,6 +25,12 @@ colunas = [
 df = df[colunas]
 
 # %%
+# Maneira de colocar as colunas em ordem alfabética
+ordem_alfabetica = df.columns.tolist()
+ordem_alfabetica.sort()
+df[ordem_alfabetica]
+
+# %%
 # Cria um dataframe novo com 100 dados aleatórios do antigo
 df_sample = df.sample(100)
 
@@ -34,5 +40,15 @@ df_sample.iloc[0]
 # %%
 df.loc[33:384]
 
+df.loc[[33,384]]
+
 # %%
 df_sample.iloc[0:10][['idPedido', 'dtPedido']]
+
+# %%
+rio = df_sample["descUF"] == "Rio de Janeiro"
+df_sample[rio][['idPedido','dtPedido','txtRecado']]
+# rio[['idPedido','dtPedido','txtRecado']]
+
+# o iloc pega o elemento que está na posiçao, por exemplo, iloc[5] pega o elemento na posicao 5 do dataframe
+# o loc pega o elemento que tem o indice, por exemplo, loc[5] pega o elemento que tem indice 5 no dataframe
